1daa7a6768d442c6933f641bb8e4145db24031cc
vincepr <vinceprinceking@gmail.com>
2023-05-22 09:34:07 +0200

    added optional flags to change port and the path to the ./public folder

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # Documentation is a work in progress
 
+## Building and running the Docker image
+- `git clone https://github.com/vincepr/go_micmute_server.git`
+- `cd go_micmute_server`
+- `sudo docker build -t go_auth_proxy .`
+- `sudo docker run -it --rm -p 3003:3003 go_auth_proxy`
+
+now we can access it in localhost:3003
+
+If we would want to expose it in our vps we could do so with--network="host" . Or we use something like nginx (see below)
+- `sudo docker run -it --rm --network="host" -p 3003:3003 go_auth_proxy`
+
+If we want it to autostart after reboots we can add the `-d --restart unless-stopped` flag:
+- `sudo docker run -it -d --restart unless-stopped -p 3003:3003 go_auth_proxy`
+
 ## Https via nginx & certbot:
 `sudo nano /etc/nginx/sites-available/mic.vprobst.de.conf`
 ```
@@ -24,5 +38,3 @@ sudo nginx -t
 systemctl status nginx sudo systemctl restart nginx
 ```
 - afterwards run certbot
-
-## Building and running with Docker image
--- src/main.go
+++ src/main.go
@@ -5,9 +5,16 @@
 package main
 
 import (
+	"flag"
 	"context"
 	"log"
 	"net/http"
+)
+
+// Optional Flags, with default values if not set.
+var (
+	PORT = flag.String("port", "3003", "the port the server listens to")
+	PATH_TO_PUBLICFOLDER = flag.String("files", "/var/www/go_micmute_server", "expected path to the /public folder with index.html")
 )
 
 func main() {
@@ -16,14 +23,18 @@ func main() {
 	ctx, cancel := context.WithCancel(rootCtx)
 	defer cancel()
 
+	flag.Parse()
+	log.Println("Serving on Port: "+*PORT)
+	log.Println("Expect files at in:"+*PATH_TO_PUBLICFOLDER)
+
 	initApi(ctx)
-	log.Fatal(http.ListenAndServe(":3003", nil))
+	log.Fatal(http.ListenAndServe(":"+*PORT, nil))
 }
 
 func initApi(ctx context.Context) {
 	manager := NewManager(ctx)
 
-	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.Handle("/", http.FileServer(http.Dir(*PATH_TO_PUBLICFOLDER)))
 	http.HandleFunc("/login_receiver", manager.loginReceiverHandler)
 	http.HandleFunc("/receiver", manager.serveReceiverHandler)
 	http.HandleFunc("/controller", manager.ControllerRequestHandler)
